5abcd09079232f3bbdf06ec8201bcf32f4b47c45
Anmol Raj <course.anmolraj@gmail.com>
2022-01-18 15:15:47 +0530

    hola

--- src/Screens/SubScreens/Dashboard.jsx
+++ src/Screens/SubScreens/Dashboard.jsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
-import Data from '../Data/SampleCourse';
-import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-import axios from 'axios';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 
 function Course({st}) {
 
-    
+    const Data =[{title: 'Total Subscription', val :{ a:'52,125' , b:'48.4%'} , ico:{a:<PersonAddIcon /> , val: 0}},
+                 {title: 'Order Status', val :{ a:'45,415' , b:'11.4%'} , ico:{a:<ShoppingCartIcon /> , val: 0}},
+                 {title: 'Monthly Earnings', val :{ a:'52,453,48' , b:'+2.4%'} , ico:{a:<PermIdentityIcon /> , val: 1}}]
+
+
+
+    const Card = ({item}) => (<div className={`shadow-lg justify-between py-4 px-4 lg:my-6 rounded-xl bg-gray-900 flex flex-row ${item.y === 0 ? ' text-blue-400' : ''} ${item.y === 1 ? ' text-yellow-400' : ''} ${item.y === 2 ? ' text-purple-500' : ''}`}>
+        <div >
+            <p>{item.x.title}</p>
+            <p>{item.x.ico.a}</p>
+        </div>
+        <div>
+            <p className=' text-right'>{item.x.val.a}</p>
+            <p>{item.x.val.b}<span className={` text-xl -mt-1 ${item.x.ico.val ? ' text-green-500' : ' text-red-600'}`}><ArrowCircleUpIcon fontSize='inherit' /></span></p>
+        </div>
+    </div>)
     
 
 
@@ -18,10 +31,36 @@ function Course({st}) {
 
 
     return (
-        <div className=''>
-           {/* dashboard */}
-dashboard
+        <div className='  h-screen bg-indigo-50 w-full'>
+            <hr className=' pt-12' />
+            <div className=' ml-8  '>
+                <div>
+                <p className=' capitalize customtextcolor font-semibold text-3xl'>Dashboard</p>
+                <p className='capitalize text-gray-400 my-3 text-lg '>Compare to last month</p>
+                <div className=' flex w-full flex-col mr-8 lg:mr-0 lg:flex-row'>
+                <div className=' grid  grid-cols-2 lg:w-4/6 gap-5 lg:grid-cols-3'>
+                 {Data.map((x,y)=> <Card key={y} item={{x,y}} />)}
 
+                </div>
+                <div className='py-4 w-full mt-8 lg:mt-6  px-4 lg:my-6 lg:w-2/6 shadow-2xl rounded-xl bg-gray-900 lg:mx-8 '>
+                    <p className=' text-center w-full text-red-500'>Monthly Earning</p>
+                    <div className=' flex flex-row w-full justify-between pt-4'>
+                        <div>
+                            <p className=' text-white text-xl'>3654</p> <p className=' text-gray-400'>Market Place</p>
+                        </div>
+                        <div>
+                            <p className=' text-white text-xl'>954</p> <p className=' text-gray-400'>Last Week </p>
+                        </div>
+                        <div>
+                            <p className=' text-white text-xl'>8464</p> <p className=' text-gray-400'>Last Month</p>
+                        </div>
+
+                    </div>
+                </div>
+                </div>
+                </div>
+                
+            </div>
         </div>
     )
 }
--- src/Screens/SubScreens/Reports.jsx
+++ src/Screens/SubScreens/Reports.jsx
@@ -1,10 +1,147 @@
 import React from 'react'
 
 function Reports() {
+    function Searchbar(){
+        return(<div class="pt-2 relative mx-auto text-gray-600">
+        <input class="border-2 w-full border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          type="search" name="search" placeholder="Search" />
+        <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+          <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+            xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
+            viewBox="0 0 56.966 56.966" style={{enableBackground:"new 0 0 56.966 56.966"}}   space="preserve"
+            width="512px" height="512px">
+            <path
+              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+          </svg>
+        </button>
+      </div>);
+    }
     return (
-        <div>
-            Reports
+        <div className=' shadow-lg w-full m-4 p-8'>
+        <p className=' font-black text-3xl'> Billing and Balance</p>
+        <form className=' mt-6'>
+            <div className=' grid grid-cols-3 gap-8'>
+            <div><input placeholder='User Id' className=' text-center bg-transparent border-b hover:border-b-2 py-4 w-full  hover:border-gray-900 border-gray-600 cursor-pointer' /></div>
+            <div><input placeholder='From Date' className=' text-center bg-transparent border-b hover:border-b-2 py-4 w-full  hover:border-gray-900 border-gray-600 cursor-pointer' /></div>
+            <div><input placeholder='To Date' className=' text-center bg-transparent border-b hover:border-b-2 py-4 w-full  hover:border-gray-900 border-gray-600 cursor-pointer' /></div>
+            <div><input placeholder='Status' className=' text-center bg-transparent border-b hover:border-b-2 py-4 w-full  hover:border-gray-900 border-gray-600 cursor-pointer' /></div>
+            <div><input placeholder='Product ID' className=' text-center bg-transparent border-b hover:border-b-2 py-4 w-full  hover:border-gray-900 border-gray-600 cursor-pointer' /></div>
+            <div><input placeholder='Transaction ID' className=' text-center bg-transparent border-b hover:border-b-2 py-4 w-full  hover:border-gray-900 border-gray-600 cursor-pointer' /></div>
+
+            </div>
+            <button className=' bg-green-500 text-white px-8 py-1 mt-6 font-medium rounded-md'> Submit</button>
+        </form>
+
+{/*  */}
+<div className=" overflow-x-auto w-full  pt-8">
+        <div className="py-2 align-middle bg-white rounded-2xl inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b pb-32 bg-white border-gray-200 sm:rounded-lg">
+          <div className=' mx-4 flex flex-row justify-between mb-8 mt-2 items-center '>
+            <div className=' text-2xl text-gray-600'>Report Details:</div>
+            <div className='lg:w-1/3'>
+            <Searchbar />
+            </div>
+          </div>
+            <table className="min-w-full divide-y   divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left w-min text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Actions
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                   Invoice ID
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    User
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Product Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Type
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Amount
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Description
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Added Date
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                
+                    <tr>
+                    <td className="pl-6 py-4 whitespace-nowrap">
+                      <div className="flex space-x-2 w-min">
+                        <button className=' text-sm px-8 bg-gray-300 py-1 rounded-md'>Edit</button>
+                        <button className=' text-sm px-8 bg-red-500 text-white  py-1 rounded-md'>Delete</button>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">pampam</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm  text-gray-900">dampam</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm  text-gray-900">boom</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm  text-gray-900">awooo</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm  text-gray-900">awooo</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm  text-gray-900">awooo</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm  text-gray-900">awooo</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm  text-gray-900">01.12.2022</div>
+                    </td>
+                  </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+      </div>
+{/*  */}
+    </div>
     )
 }
 
